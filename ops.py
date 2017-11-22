cd6b1aea957c68e6a4846bf3275efb32049114eb
Jill-Jênn Vie <jill-jenn.vie@riken.jp>
2017-11-22 16:30:52 +0900

    Replace with factorization machine

--- ops.py
+++ ops.py
@@ -4,16 +4,16 @@ import tensorflow as tf
 def inference_svd(user_batch, item_batch, user_num, item_num, dim=5, device="/cpu:0"):
     with tf.device("/cpu:0"):
         bias_global = tf.get_variable("bias_global", shape=[])
-        w_bias_user = tf.get_variable("embd_bias_user", shape=[user_num])
-        w_bias_item = tf.get_variable("embd_bias_item", shape=[item_num])
+        w_bias_user = tf.get_variable("embd_bias_user", shape=[user_num + item_num])
+        #w_bias_item = tf.get_variable("embd_bias_item", shape=[item_num])
         bias_user = tf.nn.embedding_lookup(w_bias_user, user_batch, name="bias_user")
-        bias_item = tf.nn.embedding_lookup(w_bias_item, item_batch, name="bias_item")
-        w_user = tf.get_variable("embd_user", shape=[user_num, dim],
+        bias_item = tf.nn.embedding_lookup(w_bias_user, item_batch, name="bias_item")
+        w_user = tf.get_variable("embd_user", shape=[user_num + item_num, dim],
                                  initializer=tf.truncated_normal_initializer(stddev=0.02))
-        w_item = tf.get_variable("embd_item", shape=[item_num, dim],
-                                 initializer=tf.truncated_normal_initializer(stddev=0.02))
+        #w_item = tf.get_variable("embd_item", shape=[item_num, dim],
+        #                         initializer=tf.truncated_normal_initializer(stddev=0.02))
         embd_user = tf.nn.embedding_lookup(w_user, user_batch, name="embedding_user")
-        embd_item = tf.nn.embedding_lookup(w_item, item_batch, name="embedding_item")
+        embd_item = tf.nn.embedding_lookup(w_user, user_num + item_batch, name="embedding_item")
     with tf.device(device):
         infer = tf.reduce_sum(tf.multiply(embd_user, embd_item), 1)
         infer = tf.add(infer, bias_global)
